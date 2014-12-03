a6b4ca741fdac32bb835266099deb150d8649695
re <mutemikennedy@gmail.com>
2014-12-03 22:31:10 +0300

    Created index page for front app

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
-from main import views
+from front import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
