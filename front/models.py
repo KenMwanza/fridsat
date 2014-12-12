0ca9a671516e04916aea9cbe0f82379b23de3f74
re <mutemikennedy@gmail.com>
2014-12-12 14:05:12 +0300

    Create model Device and populate with initial test data

--- front/models.py
+++ front/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Device(models.Model):
     name = models.TextField()
     category = models.TextField(blank=True, null=True)
+    brand = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
