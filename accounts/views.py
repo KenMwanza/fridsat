5bb1cba231e379f1b7f58b6974d01cd64f5f40b6
kennedy <mutemikennedy@gmail.com>
2015-06-18 01:43:56 +0300

    Revamp login page appearance and create custom decorator

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
+from accounts.decorators import anonymous_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 
+@anonymous_required(home_url='/')
 def login(request):
     # context = RequestContext(request, {
     #     'request': request, 'user': request.user})
--- front/views.py
+++ front/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from front.forms import PostDeviceForm
 from front.models import Device, County
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     return render(request, 'front/index.html')
 
+@login_required(login_url='/login/')
 def publish(request):
     devices = Device.objects.all()
     counties = County.objects.all()
