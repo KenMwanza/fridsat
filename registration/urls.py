5a0b5762b4b26094d74c745509c787e6909ce7e0
kennedy <mutemikennedy@gmail.com>
2015-08-25 23:35:26 +0300

    add landing page

--- registration/urls.py
+++ registration/urls.py
@@ -4,6 +4,7 @@ from registration import views
 
 urlpatterns = patterns('',
     url(r'^login/$', 'registration.views.login', name='login'),
+    url(r'^promo/landing/$', 'registration.views.landing', name='landing'),
     url(r'^accounts/password-change/$',
         'django.contrib.auth.views.password_change',
         {'password_change_form': SetPasswordForm},
--- registration/views.py
+++ registration/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth import logout as auth_logout
 @anonymous_required(home_url='/')
 def login(request):
     return render(request, 'registration/login.html')
+
+@anonymous_required(home_url='/')
+def landing(request):
+    return render(request, 'registration/landing.html')
