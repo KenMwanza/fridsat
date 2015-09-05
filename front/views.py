05efe2d5d08583952273bb712167dfaa08781894
kennedy <mutemikennedy@gmail.com>
2015-09-05 22:45:47 +0300

    Add breadcrumb to business page

--- front/views.py
+++ front/views.py
@@ -28,10 +28,12 @@ def category(request, slug):
 def business(request, slug):
     business = get_object_or_404(Business, slug=slug)
     category = Category.objects.get(name=business.category)
+    county = County.objects.get(name=business.county)
     return render(request, 'front/business.html',
         {
             'business': business,
             'category': category,
+            'county': county,
         }
     )
 
