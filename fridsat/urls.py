d71a326f96cdecce30303267ac60398c0f7fc652
kennedy <mutemikennedy@gmail.com>
2015-10-18 20:35:05 +0300

    Create 404 page

--- fridsat/urls.py
+++ fridsat/urls.py
@@ -4,6 +4,8 @@ from django.contrib.sitemaps.views import sitemap
 from django.contrib.sitemaps import FlatPageSitemap
 from django.conf import settings
 from django.contrib import admin
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from cms.sitemaps import CMSSitemap
 from front.sitemaps import BusinessSitemap, CountySitemap, CategorySitemap, AreaSitemap
 
@@ -31,3 +33,16 @@ urlpatterns = patterns('',
     url(r'^blog/sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
