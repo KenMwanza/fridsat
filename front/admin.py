762729503e3b638e310fc96835df16d25d51791a
kennedy <mutemikennedy@gmail.com>
2015-11-01 22:56:04 +0300

    Add CustomBusinessGroup to admin.py

--- front/admin.py
+++ front/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from front.models import Business, County, Category, Area
+from front.models import Business, County, Category, Area, CustomBusinessGroup
 
 admin.site.register(County)
 admin.site.register(Area)
 admin.site.register(Business)
 admin.site.register(Category)
+admin.site.register(CustomBusinessGroup)
