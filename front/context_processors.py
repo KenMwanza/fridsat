31e75debd1108352fc542a91c8bd8d8933bbf7e9
kennedy <mutemikennedy@gmail.com>
2015-11-24 00:05:47 +0300

    Redesign index page

--- front/context_processors.py
+++ front/context_processors.py
@@ -1,11 +1,13 @@
-from front.models import Category, County, Area
+from front.models import Category, County, Area, CustomBusinessGroup
 
 def categories_processor(request):
 	categories = Category.objects.all()
 	areas = Area.objects.all()
+	custom_lists = CustomBusinessGroup.objects.all()
 	counties = County.objects.all()     
  	return { 
  				'categories': categories,
  			 	'areas': areas,
- 			 	'counties': counties
+ 			 	'counties': counties,
+ 			 	"custom_lists": custom_lists,
  			}
